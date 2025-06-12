29169d3b0a909db322490108f6e042c7a8210268
Gyumin Bae <bgmsound0528@gmail.com>
2025-06-12 10:39:07 +0900

    Remove interface of chat summary usecase

--- chipmunk-core/src/main/kotlin/io/github/bgmsound/chipmunk/usecase/ChatSummaryUsecase.kt
+++ chipmunk-core/src/main/kotlin/io/github/bgmsound/chipmunk/usecase/ChatSummaryUsecase.kt
@@ -2,9 +2,28 @@ package io.github.bgmsound.chipmunk.usecase
 
 import io.github.bgmsound.chipmunk.Chat
 import io.github.bgmsound.chipmunk.SummaryPR
+import io.github.bgmsound.chipmunk.GithubSummaryPRCreator
+import io.github.bgmsound.chipmunk.LLMChatSummaryAgent
+import io.github.bgmsound.chipmunk.Topic
+import io.github.bgmsound.chipmunk.TopicRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.springframework.stereotype.Component
 
-interface ChatSummaryUsecase {
+@Component
+class ChatSummaryUsecase(
+    private val llmChatSummaryAgent: LLMChatSummaryAgent,
+    private val prCreator: GithubSummaryPRCreator,
 
-    suspend fun summarizeMessage(chats: List<Chat>): SummaryPR
+    private val topicRepository: TopicRepository,
+    private val coroutineScope: CoroutineScope
+) {
+    suspend fun summarizeMessage(chats: List<Chat>): SummaryPR {
+        val llmSummary = llmChatSummaryAgent.summarizeMessage(chats)
 
+        coroutineScope.launch {
+            topicRepository.addAll(llmSummary.topics.map { Topic(it) }.toSet())
+        }
+        return prCreator.createPR(llmSummary)
+    }
 }
